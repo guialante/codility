96c1f508eb8ff6ea7a0c6f3a1a3caf6a1992cc2e
Guillermo Alvarez <guialante@gmail.com>
2017-06-19 14:22:13 -0500

    changes on fuction

--- tape_equilibrium.py
+++ tape_equilibrium.py
@@ -1,30 +1,17 @@
 """ 
-    @Guillermo Alvarez
-    task link: https://codility.com/programmers/task/tape_equilibrium/
-    test results: https://codility.com/demo/results/trainingQT48E4-KKH/#task-0
+	@Guillermo Alvarez
+	task link: https://codility.com/programmers/task/tape_equilibrium/
+	test results: https://codility.com/demo/results/trainingQT48E4-KKH/#task-0
 """
 
-
-
-def sum_array_iterating(array, index):
-    result = 0
-    for i in xrange(index, len(array)):
-        result += array[i]
-    return result
-
 def solution(A):
-    P = 1
-    res = None
-    left_result = A[P-1]
-    right_result = sum_array_iterating(A, P)
-    while P <= len(A) - 1:
-        if res is None and P == 1:
-            res = abs(left_result - right_result)
-        else:
-            left_result = left_result + A[P-1]
-            right_result = right_result - A[P-1]
-            tmp = abs(left_result - right_result)
-            if tmp < res:
-                res = tmp
-        P += 1
+    left_result = A[0]
+    right_result = sum(A[1:])
+    res = abs(left_result - right_result)
+    for p in range(2, len(A)):
+        left_result = left_result + A[p-1]
+        right_result = right_result - A[p-1]
+        tmp = abs(left_result - right_result)
+        if tmp < res:
+            res = tmp
     return res
